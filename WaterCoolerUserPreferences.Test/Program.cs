12d16397cd04e7986cd9ce74f4115e02b6f789de
Pardha-vasamsety <109412879+Pardha-vasamsety@users.noreply.github.com>
2022-08-18 16:31:03 +0530

    Project to Test Model data

---
+++ WaterCoolerUserPreferences.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Preferences.Models;
+using System.Net;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var webClient = new WebClient();
+var json = webClient.DownloadString(@"C:\Work\Projects\Watercooler\Development\Data\preferences.json");
+var preferences = JsonConvert.DeserializeObject<UserPreferences>(json);
+Console.WriteLine(preferences.topics.Count);
+foreach (var item in preferences.topics)
+{
+    Console.WriteLine(item.topic);
+}
